b33b7e892b24a13a9ec176615f2c89c110640cec
Leonardo Bork <leonardomerker@gmail.com>
2018-06-09 22:56:34 -0300

    add create deck and suffle

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,19 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+              "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds", "Stars"]
 
-  @doc """
-  Hello world.
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+  end
 
-  ## Examples
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
-      iex> Cards.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
